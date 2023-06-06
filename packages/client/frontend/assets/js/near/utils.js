9e8d018c4c0d9f7c3231078d2ed4e3c6acd126b4
github honganji <yujicavs@icloud.com>
2023-06-06 19:54:19 +0900

    complete frontend and hosted to netlify server

--- packages/client/frontend/assets/js/near/utils.js
+++ packages/client/frontend/assets/js/near/utils.js
@@ -1,6 +1,9 @@
+// 以下のように書き換えてください
 import { connect, Contract, keyStores, WalletConnection } from 'near-api-js';
 
 import getConfig from './config';
+
+const BN = require('bn.js');
 
 const nearConfig = getConfig(process.env.NODE_ENV || 'development');
 
@@ -18,7 +21,6 @@ export async function initContract() {
   // is hosted at https://wallet.testnet.near.org
   window.walletConnection = new WalletConnection(near);
 
-  // Getting the Account ID. If still unauthorized, it's just empty string
   window.accountId = window.walletConnection.getAccountId();
 
   // Initializing our contract APIs by contract name and configuration
@@ -26,10 +28,24 @@ export async function initContract() {
     window.walletConnection.account(),
     nearConfig.contractName,
     {
-      // View methods are read only. They don't modify the state, but usually return some value.
-      viewMethods: ['get_greeting'],
-      // Change methods can modify the state. But you don't receive the returned value when called.
-      changeMethods: ['set_greeting'],
+      viewMethods: [
+        'nft_metadata',
+        'nft_tokens_for_kind',
+        'nft_return_candidate_likes',
+        'check_voter_has_been_added',
+        'check_voter_has_voted',
+        'if_election_closed',
+      ],
+
+      changeMethods: [
+        'new_default_meta',
+        'nft_mint',
+        'nft_transfer',
+        'nft_add_likes_to_candidate',
+        'voter_voted',
+        'close_election',
+        'reopen_election',
+      ],
     },
   );
 }
@@ -41,21 +57,97 @@ export function logout() {
 }
 
 export function login() {
-  // Allow the current app to make calls to the specified contract on the
-  // user's behalf.
-  // This works by creating a new access key for the user's account and storing
-  // the private key in localStorage.
   window.walletConnection.requestSignIn(nearConfig.contractName);
 }
 
-export async function set_greeting(message) {
-  const response = await window.contract.set_greeting({
-    args: { message: message },
-  });
-  return response;
+export async function new_default_meta() {
+  await window.contract.new_default_meta({ owner_id: window.accountId });
 }
 
-export async function get_greeting() {
-  const greeting = await window.contract.get_greeting();
-  return greeting;
+export async function nft_mint(
+  title,
+  description,
+  media,
+  media_CID,
+  candidate_name,
+  candidate_manifest,
+  token_kind,
+  receiver_id,
+) {
+  await window.contract.nft_mint(
+    {
+      metadata: {
+        title: title,
+        description: description,
+        media: media,
+        media_CID: media_CID,
+        candidate_name: candidate_name,
+        candidate_manifest: candidate_manifest,
+        token_kind: token_kind,
+      },
+      receiver_id: receiver_id,
+    },
+    300000000000000, // attached GAS (optional)
+    new BN('1000000000000000000000000'),
+  );
+}
+
+export async function nft_transfer(receiver_id, token_id) {
+  await window.contract.nft_transfer(
+    {
+      receiver_id: receiver_id,
+      token_id: token_id,
+    },
+    300000000000000, // attached GAS (optional)
+    new BN('1'), // deposit yoctoNEAR
+  );
+}
+
+export async function nft_add_likes_to_candidate(token_id) {
+  await window.contract.nft_add_likes_to_candidate({ token_id: token_id });
+}
+
+export async function nft_metadata() {
+  const contract_metadata = await window.contract.nft_metadata();
+  return contract_metadata;
+}
+
+export async function nft_tokens_for_kind(token_kind) {
+  const tokens_list = await window.contract.nft_tokens_for_kind({
+    token_kind: token_kind,
+  });
+  return tokens_list;
+}
+
+export async function nft_return_candidate_likes(token_id) {
+  const num_of_likes = await window.contract.nft_return_candidate_likes({
+    token_id: token_id,
+  });
+
+  return num_of_likes;
+}
+
+export async function check_voter_has_been_added(voter_id) {
+  return await window.contract.check_voter_has_been_added({
+    voter_id: voter_id,
+  });
+}
+
+export async function check_voter_has_voted(voter_id) {
+  return await window.contract.check_voter_has_voted({ voter_id: voter_id });
+}
+
+export async function voter_voted(voter_id) {
+  return await window.contract.voter_voted({ voter_id: voter_id });
+}
+
+export async function if_election_closed() {
+  return await window.contract.if_election_closed();
+}
+
+export async function close_election() {
+  await window.contract.close_election();
+}
+export async function reopen_election() {
+  await window.contract.reopen_election();
 }
